bc5daf92e56809ced70d64a0036a344fb8877f99
Nathalis <36462561+Nathalis@users.noreply.github.com>
2018-07-15 12:27:52 +0200

    upload

---
+++ etc/init.d/LCD.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides: LCD
+# Required-Start: 
+# Required-Stop: 
+# Should-Start: 
+# Should-Stop: 
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start and stop LCD con2fbmap
+# Description: LCD
+### END INIT INFO
+
+con2fbmap 1 8
+
+done
+
+
